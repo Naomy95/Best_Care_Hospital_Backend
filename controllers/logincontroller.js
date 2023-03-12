f6ca61954e6a70090c13ee2dffb37afb6124c6e0
Naomy95 <zannatnaomy@gmail.com>
2023-03-12 22:39:59 +0600

    jwt authentication successful

---
+++ controllers/logincontroller.js
@@ -0,0 +1,38 @@
+const jwt = require("jsonwebtoken");
+const secretKey = "secetKey";
+
+exports.create = async (req, res) => {
+  const user = req.body;
+  jwt.sign({ user }, secretKey, { expiresIn: "86400s" }, (err, token) => {
+    res.json({ token });
+  });
+};
+
+exports.createProfile = (req, res, next) => {
+  // console.log(req.headers['authorization'])
+  const bearerHeader = req.headers["authorization"];
+  const bearer = bearerHeader.split(" ");
+  const token = bearer[1];
+  console.log(token);
+
+  jwt.verify(token, secretKey, (err, authData) => {
+    if (err) {
+      res.send({result:'Invalid Token'});
+    } else {
+      res.json({ message: "Profile accessed", authData });
+    }
+  });
+};
+
+// function verifyToken(req, res, next) {
+//   const bearerHeader = req.headers["authorization"];
+//   console.log(bearerHeader);
+//   if (typeof bearerHeader !== "undefined") {
+//     const bearer = bearerHeader(" ");
+//     const token = bearer[1];
+//     req.token = token;
+//     next();
+//   } else {
+//     res.send("Token is not valid");
+//   }
+// }
---
+++ routers/loginrouter.js
@@ -0,0 +1,12 @@
+const { Router } = require('express'); 
+const {create,createProfile}=require('../controllers/logincontroller')
+const app = Router(); 
+
+
+app.post('/login', create)
+
+app.post('/profile', createProfile)
+
+
+  
+module.exports = app;
